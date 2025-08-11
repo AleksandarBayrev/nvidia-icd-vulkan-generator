26542a1eafa4559d5512581d9acd8143ee57c1fd
Aleksandar Bayrev <aleksandarbayrev@gmail.com>
2025-08-11 16:50:47 +0300

    Update checks

--- Program.cs
+++ Program.cs
@@ -1,9 +1,14 @@
-﻿using System.Text.Json;
-using NvidiaICDVulkanGenerator;
+﻿using NvidiaICDVulkanGenerator;
 
 if (!OperatingSystem.IsLinux())
 {
     System.Console.WriteLine("This program is designed for Linux only!");
+    return;
+}
+
+if (args.Length != 1 || !Constants.GetAvailableCommands().Contains(args[0]))
+{
+    Console.WriteLine($"Available args: {string.Join(", ", Constants.GetAvailableCommands())}");
     return;
 }
 
@@ -14,12 +19,6 @@ var vulkanVersion = vulkanVersionProvider.GetVersion();
 if (vulkanVersion == null)
 {
     System.Console.WriteLine("Vulkan version for NVIDIA not found (probably no NVIDIA adapter or missing driver?)");
-    return;
-}
-
-if (args.Length != 1 || !Constants.GetAvailableCommands().Contains(args[0]))
-{
-    Console.WriteLine($"Available args: {string.Join(", ", Constants.GetAvailableCommands())}");
     return;
 }
 
