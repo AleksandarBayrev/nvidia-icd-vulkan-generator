aeb045ee978246e4f4b9df070e90480f00c13620
Aleksandar Bayrev <aleksandarbayrev@gmail.com>
2025-08-11 16:16:05 +0300

    Final touches

--- Program.cs
+++ Program.cs
@@ -61,33 +61,38 @@ if (args[0] == Constants.AvailableCommands.CreateOrUpdate)
         }
     }
 
-    var contents = JsonSerializer.Deserialize<JsonModel>(await File.ReadAllTextAsync(filePath), Options.JsonSerializerOptions);
-
-    if (contents == null)
-    {
-        System.Console.WriteLine($"Failed to parse {filePath}");
-        return;
-    }
-
-    var newModel = new JsonModel();
-    newModel.ICD.APIVersion = vulkanVersion;
-
-    if (newModel.ToString() == contents.ToString())
-    {
-        System.Console.WriteLine($"{filePath} is up to date, no changes made to it.");
-        return;
-    }
-
-    contents.ICD.APIVersion = vulkanVersion;
 
     try
     {
+        var contents = JsonSerializer.Deserialize<JsonModel>(await File.ReadAllTextAsync(filePath), Options.JsonSerializerOptions);
+
+        if (contents == null)
+        {
+            throw new NullReferenceException();
+        }
+
+        var newModel = new JsonModel();
+        newModel.ICD.APIVersion = vulkanVersion;
+
+        if (newModel.ToString() == contents.ToString())
+        {
+            System.Console.WriteLine($"{filePath} is up to date, no changes made to it.");
+            return;
+        }
+
+        contents.ICD.APIVersion = vulkanVersion;
+
         await File.WriteAllTextAsync(filePath, contents.ToJsonString());
         System.Console.WriteLine($"Successfully updated {filePath}");
     }
     catch (UnauthorizedAccessException)
     {
         System.Console.WriteLine($"The file {filePath} could not be updated due to lack of administrator priviledges. Run the command as root.");
+        return;
+    }
+    catch (Exception)
+    {
+        System.Console.WriteLine($"Failed to parse {filePath}, please remove it so the program can recreate it.");
         return;
     }
 }
